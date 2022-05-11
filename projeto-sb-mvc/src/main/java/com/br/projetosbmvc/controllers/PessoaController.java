b6245ecb991117774a6e1cd996a76431d7430e9f
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-05-11 01:12:47 -0300

    update

--- projeto-sb-mvc/src/main/java/com/br/projetosbmvc/controllers/PessoaController.java
+++ projeto-sb-mvc/src/main/java/com/br/projetosbmvc/controllers/PessoaController.java
@@ -1,11 +1,16 @@
 package com.br.projetosbmvc.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.br.projetosbmvc.model.Pessoa;
@@ -18,29 +23,65 @@ public class PessoaController {
 	PessoaRepository pessoaRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String init() {
-		return "pages/pagepessoa";
+	public ModelAndView init() {
+		ModelAndView modelAndView = new ModelAndView("pages/pagepessoa");
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		
+		return modelAndView;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
-		
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		
-		return "pages/pagepessoa";
+		ModelAndView modelAndView = new ModelAndView("pages/pagepessoa");
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		return modelAndView;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/listarpessoas")
+	@GetMapping(value = "/listarpessoas")
 	public ModelAndView pessoas() {
 		
 		ModelAndView modelAndView = new ModelAndView("pages/pagepessoa");
 		
 		List<Pessoa> listPessoas = pessoaRepository.findAll();
 		modelAndView.addObject("pessoas", listPessoas);
+		modelAndView.addObject("pessoaobj", new Pessoa());
 		
 		return modelAndView;
 		
+	}
+	
+	@GetMapping(value = "/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") String idpessoa) {
 		
+		ModelAndView modelAndView = new ModelAndView("pages/pagepessoa");
+		Optional<Pessoa> pessoa = pessoaRepository.findById(Long.parseLong(idpessoa));
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		
+		return modelAndView;
+	}
+	
+	@GetMapping(value = "/excluirpessoa/{idpessoa}")
+	public ModelAndView excluir (@PathVariable("idpessoa") String idpessoa) {
+		pessoaRepository.deleteById(Long.parseLong(idpessoa));
+		
+		ModelAndView andView = new ModelAndView("pages/pagepessoa");
+		andView.addObject("pessoaobj", new Pessoa());
+		andView.addObject("pessoas", pessoaRepository.findAll());
+		return andView;
+	}
+	
+	@PostMapping(value = "**/findbyname")
+	public ModelAndView findByName(@RequestParam(name = "nome") String nome) {
+		ModelAndView modelAndView = new ModelAndView("pages/pagepessoa");
+		
+		modelAndView.addObject("pessoas", pessoaRepository.findByName(nome));
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		
+		return modelAndView;
+	}
 		
 	}
-}
+
--- projeto-sb-mvc/src/main/java/com/br/projetosbmvc/repository/PessoaRepository.java
+++ projeto-sb-mvc/src/main/java/com/br/projetosbmvc/repository/PessoaRepository.java
@@ -1,6 +1,9 @@
 package com.br.projetosbmvc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +13,6 @@ import com.br.projetosbmvc.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
+	@Query("select p from Pessoa p where p.nome like ?1% ")
+	public List<Pessoa> findByName(String nome);
 }
