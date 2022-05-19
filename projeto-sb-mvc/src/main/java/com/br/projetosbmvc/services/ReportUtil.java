64efbe8e11e78ce984a73acfd7a89a2cff36e2de
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-05-19 01:03:51 -0300

    Criação de ralatorio pessoa

---
+++ projeto-sb-mvc/src/main/java/com/br/projetosbmvc/services/ReportUtil.java
@@ -0,0 +1,38 @@
+package com.br.projetosbmvc.services;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.stereotype.Component;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@Component
+public class ReportUtil implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	/*Retorna pdf em byte para download no navegador*/
+	public byte [] generatedReport(List listData, String nameReport, ServletContext context)throws Exception {
+		
+		/*Cria lista de dados para o relatorio baseado na lista de objetos passada*/
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(listData); 
+		
+		/*Carrega o caminho do arquivo jasper*/
+		String pathJasper = context.getRealPath("reports") + File.separator + nameReport + ".jasper";
+		
+		/*Carrega o arquivo jasper passando od dados*/
+		JasperPrint jasperPrint = JasperFillManager.fillReport(pathJasper, new HashMap(),dataSource);
+		
+		/*Exporta para byte para fazer download do PDF*/
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+	}
+	
+}	
