13fff83b4277119014dd6cc17457007a9f7db068
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-05-25 13:41:20 -0300

    Metodos ta pagina de Usuario

--- projeto-sb-mvc/src/main/java/com/br/projetosbmvc/controllers/UsuarioController.java
+++ projeto-sb-mvc/src/main/java/com/br/projetosbmvc/controllers/UsuarioController.java
@@ -2,24 +2,64 @@ package com.br.projetosbmvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.projetosbmvc.model.Usuario;
 import com.br.projetosbmvc.repository.UsuarioRepository;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	@RequestMapping("/")
-	public ModelAndView init () {
-		
+	public ModelAndView init() {
+
 		ModelAndView modelAndView = new ModelAndView("pages/usuario");
 		modelAndView.addObject("usuarios", usuarioRepository.findUsuarioInit());
-		
+		modelAndView.addObject("usuarioObj", new Usuario());
+
 		return modelAndView;
 	}
+
+	@PostMapping("**/salvar")
+	public ModelAndView salvar(Usuario usuario) {
+		ModelAndView modelAndView = new ModelAndView("pages/usuario");
+		usuarioRepository.save(usuario);
+		modelAndView.addObject("msg", "Salvo com sucesso!");
+		modelAndView.addObject("usuarios", usuarioRepository.findUsuarioInit());
+		modelAndView.addObject("usuarioObj", new Usuario());
+
+		return modelAndView;
+	}
+
+	@GetMapping("/deletar/{idUser}")
+	public ModelAndView deletar(@PathVariable(name = "idUser") Long idUser) {
+
+		usuarioRepository.deleteById(idUser);
+
+		ModelAndView modelAndView = new ModelAndView("pages/usuario");
+		modelAndView.addObject("msg", "Deletado com sucesso!");
+		modelAndView.addObject("usuarios", usuarioRepository.findUsuarioInit());
+		modelAndView.addObject("usuarioObj", new Usuario());
+
+		return modelAndView;
+	}
+
+	@GetMapping("/editar/{idUser}")
+	public ModelAndView editar(@PathVariable(name = "idUser") Long idUser) {
+		ModelAndView modelAndView = new ModelAndView("pages/usuario");
+		modelAndView.addObject("usuarioObj", usuarioRepository.findById(idUser).get());
+		modelAndView.addObject("msg", "Usuario carregado para Edição");
+		modelAndView.addObject("usuarios", usuarioRepository.findUsuarioInit());
+
+		return modelAndView;
+	}
+
 }
